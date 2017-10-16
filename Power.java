c64970a732d8201a6a58339b7bdb162f893bc237
kya1215 <keiyatdai@gmail.com>
2017-10-16 21:19:18 +0900

    list7-5

---
+++ Power.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+class Power{
+	static double power(double x, int n){
+		double tmp = 1.0;
+
+		for (int i = 1; i<=n; i++)
+			tmp *= x;
+		return tmp;
+	}
+
+	public static void main(String[] args){
+		Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("aのb乗を求めます。");
+		System.out.print("実数a：");
+		double a = stdIn.nextDouble();
+		System.out.print("整数b：");
+		int b = stdIn.nextInt();
+
+		System.out.println(a+"の"+b+"乗は"+power(a,b)+"です。");
+	}
+}
